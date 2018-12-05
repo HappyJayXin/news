e2c0941e640168a399f07129301db69a821d81a3
HappyJayXin <36494560+HappyJayXin@users.noreply.github.com>
2018-12-05 20:29:42 +0800

    update fetch

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 $(function() {
-  fetch('https://newsapi.org/v2/top-headlines?country=tw&apiKey=727212ea46e34db1ab6a32d34abf7ad5', { method: 'get' })
-    .then(res => { return res.json() })
+  const URL =
+    'https://newsapi.org/v2/top-headlines?country=tw&apiKey=727212ea46e34db1ab6a32d34abf7ad5'
+  const req = new Request(URL, { method: 'GET' })  
+
+  fetch(req)    
+    .then(res => {
+      return res.json()
+    })
     .then(json => {
-        console.log(json)
+      console.log(json)
     })
     .catch(err => {
-        alert('未取得資料!')
-        console.error('ERROR',err)
-    })    
+      alert('未取得資料!')
+      console.error('ERROR', err)
+    })
 })
