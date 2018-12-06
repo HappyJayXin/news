0b7bb52d75700262fb2ba5ae7ac7c48195b17383
Jay <4a490063@stust.edu.tw>
2018-12-06 11:21:08 +0800

    add ajax

--- js/index.js
+++ js/index.js
@@ -1,17 +1,52 @@
 $(function() {
   const URL =
     'https://newsapi.org/v2/top-headlines?country=tw&apiKey=727212ea46e34db1ab6a32d34abf7ad5'
-  const req = new Request(URL, { method: 'GET' })  
+  const req = new Request(URL, {method: 'GET'})
 
-  fetch(req)    
+  fetch(req)
     .then(res => {
       return res.json()
     })
     .then(json => {
-      console.log(json)
+      showData(json)
     })
     .catch(err => {
-      alert('未取得資料!')
+      $('.alert').removeClass('d-none')
       console.error('ERROR', err)
     })
+
+  function showData(data) {
+    if (data.status === 'ok') {
+      const newsLen = data.articles.length
+      console.log(data)
+
+      for(let a = 0; a < newsLen; a++) {
+        $('#newsList').append(
+          `<a
+          href="#"
+          class="list-group-item list-group-item-action flex-column align-items-start"
+        >
+        <div class="row">          
+          <small class="col-12">${transTime(
+            data.articles[a].publishedAt
+          )}</small>  
+          <h3 class="mb-3 col-12">${data.articles[a].title}</h3>          
+        </div>          
+          <p class="mb-1 text-truncate" style="max-width: 50rem">
+            ${data.articles[a].description}
+          </p>
+          <small class="float-right">${data.articles[a].source.name}</small>
+        </a>`
+        )
+      }      
+    }
+  }
+
+  // 轉換時間格式
+  function transTime(time) {
+    return new Date(+new Date(time) + 8 * 3600 * 1000)
+      .toISOString()
+      .replace(/T/g, ' ')
+      .replace(/\.[\d]{3}Z/, '')
+  }
 })
